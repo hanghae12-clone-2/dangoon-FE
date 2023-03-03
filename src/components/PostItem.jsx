1c6c7067645f1baa1eff9d7217f07bb0f48e5f65
Adult96 <djxor007@gmail.com>
2023-03-03 18:09:17 +0900

    feat: postItem 컴포넌트 마크업

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function PostItem({
+  post: { img, title, money, address, like, chat },
+}) {
+  return <ItemContainer></ItemContainer>;
+}
+
+const ItemContainer = styled.div``;
