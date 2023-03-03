361fed04a0d2c77cd92bd37c2d50a111652fb678
Adult96 <djxor007@gmail.com>
2023-03-03 18:18:18 +0900

    design: postItem ui 마크업

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import Img from '../elements/Img';
 
 export default function PostItem({
   post: { img, title, money, address, like, chat },
 }) {
-  return <ItemContainer></ItemContainer>;
+  return (
+    <ItemContainer>
+      <Img src={img} medium />
+      <Content>
+        <p>{title}</p>
+        <p>{money}</p>
+        <p>{address}</p>
+        <span>{like}</span>
+        <span>{chat}</span>
+      </Content>
+    </ItemContainer>
+  );
 }
 
-const ItemContainer = styled.div``;
+const ItemContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Content = styled.div``;
