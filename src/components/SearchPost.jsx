02974a9490eef45f8d2e22f79279d8b8f1f32858
Adult96 <djxor007@gmail.com>
2023-03-04 19:22:55 +0900

    feat: searchPost component 추가

---
+++ src/components/SearchPost.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import { v4 as uuidv4 } from 'uuid';
+import styled from 'styled-components';
+import PostItem from './PostItem';
+import { useInfiniteScrollQuery } from '../hooks/useInfiniteScrollQuery';
+import QUERY from '../constants/query';
+import { Link, useParams } from 'react-router-dom';
+import ROUTER from '../constants/router';
+
+export default function SearchPost() {
+  const { keyWord } = useParams();
+
+  const {
+    ref,
+    isLoading,
+    isError,
+    isFetchingNextPage,
+    hasNextPage,
+    data: posts,
+  } = useInfiniteScrollQuery(
+    [QUERY.KEY.POSTS],
+    QUERY.AXIOS_PATH.SEVER,
+    QUERY.AXIOS_PATH.HOT_POST,
+    16
+  );
+  return (
+    <>
+      {isLoading && <p>로딩중</p>}
+      {isError && <p>에러</p>}
+      {posts && (
+        <PostWrapper>
+          <PostTitle>{`키워드: ${keyWord}`}</PostTitle>
+          <PostList>
+            {posts?.pages.map(post =>
+              post.data.result.map(data => (
+                <Li key={uuidv4()}>
+                  <Link to={`${ROUTER.PATH.DETAIL}/${data.postid}`}>
+                    <PostItem post={data} />
+                  </Link>
+                </Li>
+              ))
+            )}
+          </PostList>
+          {isFetchingNextPage && hasNextPage ? (
+            ''
+          ) : (
+            <LastPageComment ref={ref}>마지막 페이지 입니다.</LastPageComment>
+          )}
+        </PostWrapper>
+      )}
+    </>
+  );
+}
+
+const PostWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  background-color: ${props => props.theme.color.sky_white};
+`;
+
+const PostTitle = styled.h1`
+  margin: 8rem 0;
+  font-size: ${props => props.theme.fontSize.large};
+`;
+
+const PostList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 65rem;
+`;
+
+const Li = styled.li`
+  margin-bottom: 5rem;
+`;
+
+const LastPageComment = styled.div`
+  margin-bottom: 1rem;
+`;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import SearchPost from '../components/SearchPost';
 
 export default function Search() {
-  return <div></div>;
+  return (
+    <SearchWrapper>
+      <SearchPost />
+    </SearchWrapper>
+  );
 }
+
+const SearchWrapper = styled.section`
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
