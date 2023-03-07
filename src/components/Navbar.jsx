72499a144b8969151b1d08e2d3522374b5ffde46
Adult96 <djxor007@gmail.com>
2023-03-07 16:08:20 +0900

    feat: 마이 메뉴 기능 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,35 @@ import Button from '../elements/Button';
 import Input from '../elements/Input';
 import Text from '../elements/Text';
 import logo from '../styles/logo';
+import { FaUserCircle } from 'react-icons/fa';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { Link, useNavigate } from 'react-router-dom';
 import ROUTER from '../constants/router';
+import Storage from '../utils/localStorage';
+import { removeCookie } from '../utils/cookie';
+import QUERY from '../constants/query';
 
 export default function Navbar() {
   const [keyWord, setKeyWord] = useState('');
+  const [showMyMenu, setShowMyMenu] = useState(false);
+
   const navigate = useNavigate();
+  const userName = Storage.getUserName();
 
   const handleSubmit = e => {
     e.preventDefault();
     if (!keyWord) return;
     navigate(`/search/${keyWord}`);
+  };
+
+  const handleShowMyMenu = () => {
+    setShowMyMenu(state => !state);
+  };
+
+  const handleLogOut = () => {
+    Storage.removeUserName();
+    removeCookie(QUERY.COOKIE.COOKIE_NAME);
+    setShowMyMenu(false);
   };
 
   return (
@@ -33,11 +50,26 @@ export default function Navbar() {
             value={keyWord}
             onChange={e => setKeyWord(e.target.value)}
           />
-          <Link to={ROUTER.PATH.LOGIN}>
-            <Button small type='button'>
-              로그인
-            </Button>
-          </Link>
+          {userName ? (
+            <ShowMyMenuContainer>
+              <Text large_medium>
+                <FaUserCircle onClick={handleShowMyMenu} />
+              </Text>
+              {showMyMenu ? (
+                <ShowMyMenu>
+                  <span onClick={handleLogOut}>로그아웃</span>
+                </ShowMyMenu>
+              ) : (
+                ''
+              )}
+            </ShowMyMenuContainer>
+          ) : (
+            <Link to={ROUTER.PATH.LOGIN}>
+              <Button small type='button'>
+                로그인
+              </Button>
+            </Link>
+          )}
         </FormContainer>
       </NavbarContainer>
     </NavbarWrapper>
@@ -89,4 +121,46 @@ const FormContainer = styled.form`
   max-width: 20rem;
   width: 100%;
   gap: 1rem;
+
+  svg {
+    color: ${props => props.theme.color.messenger};
+    cursor: pointer;
+  }
+`;
+const ShowMyMenuContainer = styled.div`
+  position: relative;
+`;
+
+const ShowMyMenu = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${props => props.theme.color.messenger};
+  border-radius: 0.5rem;
+  background-color: ${props => props.theme.color.white};
+  transform: translate(0, 4rem);
+  z-index: 1000;
+
+  span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 7rem;
+    height: 1.5rem;
+    padding: 1rem;
+    border-bottom: none;
+    cursor: pointer;
+
+    :hover {
+      background-color: ${props => props.theme.color.messenger};
+    }
+  }
+
+  span:not(:last-child) {
+    border-bottom: 1px solid ${props => props.theme.color.messenger};
+  }
 `;
